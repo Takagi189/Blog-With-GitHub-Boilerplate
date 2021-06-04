4d561723e813af8ad8938f51e5b199516028331f
Takagi189 <85328944+Takagi189@users.noreply.github.com>
2021-06-04 22:35:57 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -11,7 +11,7 @@ archives_page_size = 20
 template = {
     "name": "Galileo",
     "type": "local",
-    "path": "../Galileo"
+    "path": "../banbendalao"
 }
 enable_jsdelivr = {
     "enabled": True,
